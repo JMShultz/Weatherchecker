2abf072562b853cbdefa30e99e5e93a371060da4
Josh <joshschulze05@gmail.com>
2025-04-07 23:20:25 -0400

    update

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,10 @@
 const apiKey = '48205a00080b9db7adfd2a939418ad39';
 const baseURL='https://api.openweathermap.org/data/2.5/weather';
 
-const cityInput= document.getElementById('City-input');
-const searchBtn= document.getElementById('City-input');
-const weatherDisplay= document.getElementById('City-input');
-const errorMessage= document.getElementById('City-input');
+const cityInput= document.getElementById('city-input');
+const searchBtn= document.getElementById('btn-search');
+const weatherDisplay= document.getElementById('displayWeather');
+const errorMessage= document.getElementById('error-message');
 
 
 
@@ -19,7 +19,7 @@ const weatherIcon= document.getElementById('weatherIcon');
 
 async function fetchweather(City){
 try {
-    const responce= await fetch(`${baseURL}?q=${City}&appid=${apiKey}&units=metric`)
+    const responce= await fetch(`${baseURL}?q=${City}&appid=${apiKey}&units=imperial`)
     const data = await responce.json()
     displayWeather(data)
     errorMessage.classList.add("hidden")
@@ -30,16 +30,32 @@ try {
 }
 };
 
-fetchweather('Ohio')
-    function showError(message){
 
+    function showError(message){
+errorMessage.textContent=message
+errorMessage.classList.remove('hidden')
+weatherDisplay.classList.add('hidden')
     };
 
     function displayWeather(data){
         CityName.textContent=`Weather in ${data.name},${data.sys.country}`
         weatherDiscription.textContent=`condition:${data.weather[0].description}`
-        Temp.textContent=`Temperature:${data.main.temp} C`
+        Temp.textContent=`Temperature:${data.main.temp} F`
         Humidity.textContent=`Humidity:${data.main.humidity}`
-        windSpeed.textContent=`WindSpeed:${data.wind.speed}`
+        windSpeed.textContent=`Wind Speed:${data.wind.speed}`
 
     };
+
+    searchBtn.addEventListener('click',() => {
+        const city=cityInput.value.trim()
+
+        if(city){
+            fetchweather(city)
+            cityInput.value=''
+
+        }else{
+            showError("Please enter city")
+        }
+
+
+    })
